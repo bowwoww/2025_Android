0c62b1c7b5f1798e7f946529fc906a886611932a
Bowwow <rabbit88823@gmail.com>
2025-06-04 11:37:20 +0800

    練習了Relative Layout

--- app/src/main/java/com/example/application01/MainActivity.java
+++ app/src/main/java/com/example/application01/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.application01;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckedTextView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,16 +14,24 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+//    private CheckedTextView mCheckedTextView01;
+    private RadioButton[] obejctButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        EdgeToEdge.enable(this);
-        setContentView(R.layout.layout_button);
+        setContentView(R.layout.activity_releative_layout);
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
 //            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
 //            return insets;
 //        });
+
+//        mCheckedTextView01 = findViewById(R.id.CheckedTextView1);
+//        mCheckedTextView01.setChecked(true);
+//        mCheckedTextView01.setOnClickListener(v -> mCheckedTextView01.toggle());
+
 
     }
 
