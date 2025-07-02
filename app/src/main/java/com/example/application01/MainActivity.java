fcfa1c8aabb8f412c3ad54d8e2956f3689fad676
Bowwow <rabbit88823@gmail.com>
2025-07-02 11:37:16 +0800

    grid / frame / table / constraint

--- app/src/main/java/com/example/application01/MainActivity.java
+++ app/src/main/java/com/example/application01/MainActivity.java
@@ -15,13 +15,13 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
 //    private CheckedTextView mCheckedTextView01;
-    private RadioButton[] obejctButton;
+//    private RadioButton[] obejctButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_releative_layout);
+        setContentView(R.layout.homework01_constraint);
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
 //            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
